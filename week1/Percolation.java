1ff173f10c7f222c46205ad90e9899d4f04f2ebb
khoaHyh <khoahuynh.toronto@gmail.com>
2021-01-01 20:57:56 -0500

    Feat: Add rough draft of Percolation
    Need to Update PercolationStats to display data.

--- week1/Percolation.java
+++ week1/Percolation.java
@@ -1,39 +1,46 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
-
-// useful WeightedQuickUnionUF methods
-//  count
-//  find
-//  connected
-//  validate
-//  union
-
 
 public class Percolation {
-    private static int[] parent;
-    private static int[] gridSize;
-    private static int totalSites;
+    private static WeightedQuickUnionUF connectedGrid;
+    private static boolean[] openSites;
+    private static int gridSize, virtualTop, virtualBot, totalOpenSites;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-        if (n <= 0) throw new IllegalArgumentException();
-        totalSites = n;
-        parent = new int[n];
-        gridSize = new int[n];
+        if (n <= 0)
+            throw new IllegalArgumentException();
 
+        gridSize = n;
+        connectedGrid = new WeightedQuickUnionUF(n * n + 2);
+        openSites = new boolean[n * n];
+
+        // Initialize whole grid to be blocked, if other libraries were allowed,
+        // Array.fill() would be a clean way to write this.
+        for (int i = 0; i < openSites.length; i ++) {
+            openSites[i] = false;
+        }
+
+        // Initialize virtual top and bottom sites
+        virtualTop = (n * n);
+        virtualBot = (n * n) + 1;
+
+        // Connect all top sites of N by N grid to virtual top site
+        for (int i = 0; i < n; i++)
+            connectedGrid.union(virtualTop, i);
+
+        // Connect all bottom sites of N b N grid to virtual bottom site
+        for (int i = openSites.length - 1; i >= openSites.length - n; i--)
+            connectedGrid.union(virtualBot, i);
     }
 
     // Map 2D pair to a 1D union-find object index
     private static int xyTo1D(int row, int col) {
-        return gridSize[row] = col;
+        return ((row - 1) * gridSize) + (col - 1);
     }
 
     // Throw an exception for invalid indices
     private static void validateIndices(int row, int col) {
-        if (row < 1 || row > totalSites || col < 1 || col > totalSites   ) {
+        if (row < 1 || row > gridSize || col < 1 || col > gridSize) {
             throw new IllegalArgumentException();
         }
     }
@@ -41,44 +48,44 @@ public class Percolation {
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         validateIndices(row, col);
+        int index1D = xyTo1D(row, col);
+
+        // Mark the site as open if isn't already
+        if (!openSites[index1D]) {
+            totalOpenSites++;
+            openSites[index1D] = true;
+
+            // Link the site in question to its open neighbors
+            if (row + 1 <= gridSize)
+                connectedGrid.union(index1D, index1D + gridSize);
+            if (row - 1 > 0)
+                connectedGrid.union(index1D, index1D - gridSize);
+            if (col + 1 <= gridSize)
+                connectedGrid.union(index1D, index1D + 1);
+            if (col - 1 > 0)
+                connectedGrid.union(index1D, index1D - 1);
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         validateIndices(row, col);
-        return true;
+        return openSites[xyTo1D(row, col)];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         validateIndices(row, col);
-        return true;
+        return connectedGrid.find(xyTo1D(row, col)) == virtualTop;
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        // TODO
-        return 1;
+        return totalOpenSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        // TODO
-        return false;
-    }
-
-    // test client (optional)
-    public static void main(String[] args) {
-        // Test command-line arguments
-        if (args.length != 2) {
-            throw new NullPointerException("2 command-line arguments are required.");
-        } else {
-            System.out.println(args[0]);
-            System.out.println(args[1]);
-        }
-
-        // Test private method
-        Percolation test = new Percolation(40);
-        System.out.println(xyTo1D(12,  23));
+        return connectedGrid.find(virtualBot) == virtualTop;
     }
 }
--- week1/PercolationStats.java
+++ week1/PercolationStats.java
@@ -1,7 +1,18 @@
-public class PercolationStats {
-    // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials) {
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    private static int n;
+    private static int T;
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int N, int trials) {
+        if (N <= 0 || trials <= 0)
+            throw new IllegalArgumentException();
+
+        n = N;
+        T = trials;
     }
 
     // sample mean of percolation threshold
@@ -11,7 +22,12 @@ public class PercolationStats {
 
     // sample standard deviation of percolation threshold
     public double stddev() {
+        if (T == 1) {
+            return Double.NaN;
+        }
+        else {
 
+        }
     }
 
     // low endpoint of 95% confidence interval
@@ -26,6 +42,18 @@ public class PercolationStats {
 
     // test client (see below)
     public static void main(String[] args) {
+        int n;
+        int T;
+        if (args.length != 2) {
+            throw new NullPointerException("2 command-line arguments are required.");
+        } else {
+            n = Integer.parseInt(args[0]);
+            T = Integer.parseInt(args[1]);
+        }
+
+        PercolationStats test = new PercolationStats(n, T);
+        Percolation percolationObj = new Percolation(n);
+
 
     }
 }
