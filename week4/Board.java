9bd2d1388129b426191a5506e64f48ff8673bdcb
khoaHyh <khoahuynh.toronto@gmail.com>
2021-01-23 00:07:32 -0500

    Feat: Update Board.java to have 1D array
    Implementation is not 100% correct.

--- week4/Board.java
+++ week4/Board.java
@@ -1,20 +1,20 @@
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdRandom;
-
 import java.util.Arrays;
 
 public class Board {
     // Board dimension
     private final int n;
     // Independent copy of tiles array
-    private final byte[][] copyArr;
+    private final char[] copyArr;
+    // length of 1D tiles array (n^2)
+    private final int nSquared;
     // Stores position of the blank square
-    private int rowBs = 0;
-    private int colBs = 0;
+    private int blankIndex;
 
     // check if the tile is in the correct place
-    private boolean inWrongPlace(int row, int col) {
-        return this.copyArr[row][col] != col + 1 + (row * n);
+    private boolean inWrongPlace(int index) {
+        return this.copyArr[index] != index + 1;
     }
 
     // utility function to calculate horizontal or
@@ -25,48 +25,47 @@ public class Board {
         else return 0;
     }
 
-    // convert int to byte
-    private byte intToByte(int x) {
-        return Byte.parseByte(Integer.toString(x));
-    }
-
     // returns a new array with swapped elements
-    private int[][] swap(int a, int b, int x, int y) {
-        int[][] copy = copy2D(this.copyArr);
-        int temp = copy[a][b];
-        copy[a][b] = copy[x][y];
-        copy[x][y] = temp;
+    private int[][] swap(int a, int b) {
+        int[][] copy = charToIntArr(this.copyArr);
+        int temp = copy[getRow(a)][getCol(a)];
+        copy[getRow(a)][getCol(a)] = copy[getRow(b)][getCol(b)];
+        copy[getRow(b)][getCol(b)] = temp;
 
         return copy;
     }
 
-    // copy2D array
-    private int[][] copy2D(byte[][] arr) {
+    private int[][] charToIntArr(char[] arr) {
         int[][] newArr = new int[n][n];
-
         for (int row = 0; row < n; row++)
-            for (int col = 0; col < n; col++) {
-                newArr[row][col] = arr[row][col];
-            }
+            for (int col = 0; col < n; col++)
+                newArr[row][col] = arr[row * n + col];
 
         return newArr;
+    }
+
+    // get the row from a 1D array index
+    private int getRow(int index) {
+        return (index - getCol(index)) / n;
+    }
+
+    // get the column from a 1D array index
+    private int getCol(int index) {
+        return index % n;
     }
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
         n = tiles.length;
-
-        this.copyArr = new byte[n][n];
+        nSquared = n*n;
+        this.copyArr = new char[n*n];
         for (int row = 0; row < n; row++)
             for (int col = 0; col < n; col++) {
-                if (copyArr[row][col] == 0) {
-//                    blankSquare[0] = row;
-//                    blankSquare[1] = col;
-                    rowBs = row;
-                    colBs = col;
+                if (tiles[row][col] == 0) {
+                    blankIndex = row * n * col;
                 }
-                this.copyArr[row][col] = intToByte(tiles[row][col]);
+                this.copyArr[row * n + col] = (char) tiles[row][col];
             }
     }
 
@@ -74,9 +73,10 @@ public class Board {
     public String toString() {
         StringBuilder s = new StringBuilder();
         s.append(n).append("\n");
+        int length = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                s.append(String.format("%2d ", this.copyArr[i][j]));
+                s.append(String.format("%2d ", (int) this.copyArr[length++]));
             }
             s.append("\n");
         }
@@ -91,11 +91,8 @@ public class Board {
     // number of tiles out of place
     public int hamming() {
         int count = 0;
-        for (int row = 0; row < n; row++)
-            for (int col = 0; col < n; col++) {
-                if (inWrongPlace(row, col) && this.copyArr[row][col] != 0)
-                    count ++;
-            }
+        for (int i = 0; i < n; i++)
+            if (inWrongPlace(i) && this.copyArr[i] != 0) count ++;
 
         return count;
     }
@@ -103,30 +100,28 @@ public class Board {
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
         int sum = 0;
-
-        for (int row = 0; row < n; row++)
+        for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
-                int tile = this.copyArr[row][col];
-                int goalRow;
+                int tile = (int) this.copyArr[row * n + col];
+                if (inWrongPlace(tile) && tile != 0) {
+                    int goalRow;
+                    if (tile % n != 0) goalRow = Math.floorDiv(tile, n);
+                    else goalRow = Math.floorDiv(tile, n) - 1;
+                    int goalCol = tile - (goalRow * n) - 1;
 
-                if (tile % n != 0) goalRow = Math.floorDiv(tile, n);
-                else goalRow = Math.floorDiv(tile, n) - 1;
-
-                int goalCol = tile - (goalRow * n)  - 1;
-                if (inWrongPlace(row, col) && tile != 0) {
-                    // Calculate vertical + horizontal distance
+                    // add the combined vertical and horizontal distances to the sum
                     sum += addToSum(col, goalCol) + addToSum(row, goalRow);
                 }
             }
-
+        }
         return sum;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
         // To save time, check if the last tile on the board isn't equal to 0
-        if (this.copyArr[n-1][n-1] != 0) return false;
-        return hamming() == 0;
+        if (this.copyArr[nSquared - 1] != 0) return false;
+        return this.hamming() == 0;
     }
 
     // does this board equal y?
@@ -135,29 +130,27 @@ public class Board {
         if (y == null) return false;
         if (y.getClass() != this.getClass()) return false;
         Board that = (Board) y;
-        return Arrays.deepEquals(this.copyArr, that.copyArr);
+        return Arrays.equals(this.copyArr, that.copyArr);
     }
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
         Stack<Board> neighbors = new Stack<>();
 
-        if (rowBs > 0) neighbors.push(new Board(swap(rowBs, colBs, rowBs - 1, colBs)));
-        if (rowBs < n - 1) neighbors.push(new Board(swap(rowBs, colBs, rowBs + 1, colBs)));
-        if (colBs > 0) neighbors.push(new Board(swap(rowBs, colBs, rowBs, colBs - 1)));
-        if (colBs < n - 1) neighbors.push(new Board(swap(rowBs, colBs, rowBs, colBs + 1)));
+        if (getRow(blankIndex) > 0) neighbors.push(new Board(swap(blankIndex, blankIndex - n)));
+        if (getRow(blankIndex) < n - 1) neighbors.push(new Board(swap(blankIndex, blankIndex + n)));
+        if (getCol(blankIndex) > 0) neighbors.push(new Board(swap(blankIndex - 1, blankIndex)));
+        if (getCol(blankIndex) < n - 1) neighbors.push(new Board(swap(blankIndex + 1, blankIndex)));
 
         return neighbors;
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-        for (int row = 0; row < n; row++)
-            for (int col = 0; col < n - 1; col++) {
-                if (this.copyArr[row][col] != 0 && this.copyArr[row][col + 1] != 0) {
-                        return new Board(swap(row, col, row, col + 1));
-                    }
-                }
+        for (int i = 0; i < nSquared - 1; i++)
+            if (this.copyArr[i] != 0 && this.copyArr[i + 1] != 0)
+                return new Board(swap(i, i+1));
+
         throw new RuntimeException("Invalid board, too many zeroes.");
     }
 
